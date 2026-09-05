84bb1431e2d1e63a6d2cd3c4004fee5f82b915ee
Jacob Mitchell <jacobemitchell14@gmail.com>
2026-09-05 09:47:29 -0600

    king piece

--- shared/src/main/java/chess/ChessPiece.java
+++ shared/src/main/java/chess/ChessPiece.java
@@ -53,8 +53,33 @@ public class ChessPiece {
      */
     public Collection<ChessMove> pieceMoves(ChessBoard board, ChessPosition myPosition) {
         Collection<ChessMove> moves = new ArrayList<>();
+        if (type == PieceType.KING){
+            kingMoves(board, myPosition, moves);
+        }
         return moves;
     }
+
+    private void kingMoves(ChessBoard board, ChessPosition myPosition, Collection<ChessMove> moves){
+        int[][] directions = {
+                {1, 0}, {-1, 0}, {0, 1}, {0, -1}, {1, 1}, {1, -1}, {-1, 1}, {-1, -1}
+        };
+        for (int[] direction : directions){
+            int row = myPosition.getRow() + direction[0];
+            int col = myPosition.getColumn() + direction[1];
+            ChessPosition newPosition = new ChessPosition(row, col);
+            if(offBoard(newPosition)) {
+                continue;
+            }
+            if(openSpace(board, newPosition)){
+                moves.add(new ChessMove(myPosition, newPosition,null));
+            }
+            else if (otherColor(board, newPosition)){
+                moves.add(new ChessMove(myPosition, newPosition, null));
+            }
+        }
+    }
+
+
 
     private boolean openSpace(ChessBoard board, ChessPosition position) {
         return position.getRow() >= 1 && position.getRow() <= 8 &&
@@ -70,6 +95,12 @@ public class ChessPiece {
     private boolean inBounds(ChessPosition position) {
         return position.getRow() >= 1 && position.getRow() <= 8 &&
                 position.getColumn() >= 1 && position.getColumn() <= 8;
+    }
+
+    private boolean offBoard(ChessPosition position){
+        int row = position.getRow();
+        int col = position.getColumn();
+        return row < 1 || row > 8 || col < 1 || col > 8;
     }
 
 }
