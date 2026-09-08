cdf4d0a5c6d9a7ad34d73e2447b90873eef47f4f
Jacob Mitchell <jacobemitchell14@gmail.com>
2026-09-07 20:11:48 -0600

    override ChessPosition

--- shared/src/main/java/chess/ChessPosition.java
+++ shared/src/main/java/chess/ChessPosition.java
@@ -1,5 +1,7 @@
 package chess;
 
+import java.util.Objects;
+
 /**
  * Represents a single square position on a chess board
  * <p>
@@ -30,5 +32,14 @@ public class ChessPosition {
      */
     public int getColumn() {
         return col;
+    }
+    @Override
+    public boolean equals(Object pos){
+        if (this == pos)
+            return  true;
+        else if (pos == null || getClass() != pos.getClass())
+            return  false;
+        ChessPosition that = (ChessPosition) pos;
+            return  row == that.row && col == that.col;
     }
 }
